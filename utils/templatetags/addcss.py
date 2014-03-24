bce65b6d737afb969a9752dc4269d42a6cac921e
Tom Hayward <esarfl+github@gmail.com>
2014-03-24 15:37:59 -0700

    addcss templatetag: ignore errors.

--- utils/templatetags/addcss.py
+++ utils/templatetags/addcss.py
@@ -6,4 +6,7 @@ register = template.Library()
 
 @register.filter(name='addcss')
 def addcss(field, css):
-   return field.as_widget(attrs={"class":css})
+    try:
+        return field.as_widget(attrs={"class":css})
+    except:
+        return field
